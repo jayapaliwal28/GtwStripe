aba4659c949a12a60e308ce7bf55a5885ce5cbf5
jadavsushil <jadavsushil@gmail.com>
2014-10-17 11:11:10 +0530

    - remove extra code

--- Console/Command/SetupShell.php
+++ Console/Command/SetupShell.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * @property Currency $Currency
- * @property Exchange $Exchange
- * @property Simulation $Simulation
- * @property Portfolio $Portfolio
- * @property Historical $Historical
- * @property Dividend $Dividend
- * @property Journal $Journal
- * @property Title $Title
- */
+
 class SetupShell extends AppShell {
     public $uses = array('GtwStripe.TransactionType','GtwStripe.Transaction');
 	
